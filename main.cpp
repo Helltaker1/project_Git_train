753d37fe189eebadafa4628e5426324500944b89
235909 <245826@edu.p.lodz.pl>
2022-11-06 10:12:52 +0100

    02 zmiana mnożenia w metodzie Policz

--- main.cpp
+++ main.cpp
@@ -5,6 +5,6 @@ int Policz(int a,int b){
 }
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    std::cout << "W drugim branchu; 2*3=" << Policz(2,3) << std::endl;
+    std::cout << "W drugim branchuu; 2*3=" << Policz(2,3) << std::endl;
     return 0;
 }
